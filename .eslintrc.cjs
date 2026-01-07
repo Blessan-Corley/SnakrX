678ac9db719f6de975075419e99a2cbb9a94cb8b
Blessan-Corley <blessancorley@gmail.com>
2026-01-07 10:27:41 +0530

    chore: add prettier and eslint config

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -5,29 +5,46 @@ module.exports = {
     'eslint:recommended',
     'plugin:react-hooks/recommended',
     'plugin:react/recommended',
-    'plugin:react/jsx-runtime'
+    'plugin:react/jsx-runtime',
+    'prettier',
   ],
-  ignorePatterns: ['dist', '.eslintrc.cjs'],
+  ignorePatterns: ['dist', 'coverage', '.eslintrc.cjs'],
   parserOptions: {
     ecmaVersion: 'latest',
     sourceType: 'module',
     ecmaFeatures: {
-      jsx: true
-    }
+      jsx: true,
+    },
   },
   plugins: ['react-refresh'],
+  overrides: [
+    {
+      files: ['functions/**/*.js', 'playwright.config.js', 'e2e/**/*.js'],
+      env: { node: true, es2020: true },
+    },
+    {
+      files: ['src/**/*.test.js', 'src/**/*.test.jsx'],
+      globals: {
+        describe: 'readonly',
+        it: 'readonly',
+        expect: 'readonly',
+        vi: 'readonly',
+        beforeEach: 'readonly',
+        afterEach: 'readonly',
+        beforeAll: 'readonly',
+        afterAll: 'readonly',
+      },
+    },
+  ],
   rules: {
-    'react-refresh/only-export-components': [
-      'warn',
-      { allowConstantExport: true },
-    ],
+    'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
     'react/prop-types': 'off',
     'react/react-in-jsx-scope': 'off',
-    'no-unused-vars': ['warn', { argsIgnorePattern: '^_' }]
+    'no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
   },
   settings: {
     react: {
-      version: '18.2'
-    }
-  }
-}
+      version: '18.2',
+    },
+  },
+};
